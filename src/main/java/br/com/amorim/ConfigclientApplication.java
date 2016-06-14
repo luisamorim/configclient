3955bd804a03f59eec14a5cf1c38d1a5e9f3ab19
luishamorim <kicoamorim@gmail.com>
2016-06-14 13:30:15 -0300

    configclient ok

--- src/main/java/br/com/amorim/ConfigclientApplication.java
+++ src/main/java/br/com/amorim/ConfigclientApplication.java
@@ -1,7 +1,11 @@
 package br.com.amorim;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigclientApplication {
@@ -9,4 +13,18 @@ public class ConfigclientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
 	}
+}
+
+@RefreshScope
+@RestController
+class MessageRestController{
+	
+	@Value("${message}")
+	private String message;
+	
+	@RequestMapping("/message")
+	String msg(){
+		return this.message;
+	}
+	
 }
